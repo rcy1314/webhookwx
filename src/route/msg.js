98920964f14f8d45d52a1b6ed43dded430074a32
noise <1129159201@qq.com>
2023-12-26 17:48:00 +0800

    增加区分单人对话还是群组对话

--- src/route/msg.js
+++ src/route/msg.js
@@ -1,42 +1,42 @@
-const Service = require('../service')
-const Middleware = require('../middleware')
-const Utils = require('../utils/index')
+const Service = require('../service');
+const Middleware = require('../middleware');
+const Utils = require('../utils/index');
 
 module.exports = function registerPushHook({ app, bot }) {
   // 定义发送者的name值函数
-  const getSenderName = (to) => {
+  const getSenderName = (to, isRoom) => {
     // 支持jsonLike传递备注名 {alias: 123}
     if (/{\s*"?'?alias"?'?\s*:[^}]+}/.test(to)) {
       try {
-        const parsedTo = Utils.parseJsonLikeStr(to)
-        return parsedTo.alias
+        const parsedTo = Utils.parseJsonLikeStr(to);
+        return parsedTo.alias;
       } catch (e) {
-        throw new Error(`to 参数发消息给备注名， json string 格式不正确`)
+        throw new Error(`to 参数发消息给备注名， json string 格式不正确`);
       }
     } else {
-      return to
+      return to;
     }
-  }
+  };
 
   // 处理 POST 请求
   app.post(
     '/webhook/msg',
     Middleware.dynamicStorageMiddleware,
     Service.handleError(async (req, res) => {
-      let to, isRoom, content, type
-      let unValidParamsStr = ''
+      let to, isRoom, content, type;
+      let unValidParamsStr = '';
 
       // 表单传文件(暂时只用来传文件)
       if (req.is('multipart/form-data')) {
-        to = req.body.to
-        isRoom = req.body.isRoom || '0'
-        content = req.files.find((item) => item.fieldname === 'content') || {}
+        to = req.body.to;
+        isRoom = req.body.isRoom || '0';
+        content = req.files.find((item) => item.fieldname === 'content') || {};
         // 转化上传文件名中文字符但是被编码成 iso885910 的问题
         content.originalname &&
           (content.originalname = Utils.tryConvertCnCharToUtf8Char(
-            content.originalname,
-          ))
-        type = 'file'
+            content.originalname
+          ));
+        type = 'file';
 
         // 校验必填参数
         unValidParamsStr = Utils.getUnValidParamsList([
@@ -62,28 +62,30 @@ module.exports = function registerPushHook({ app, bot }) {
             type: 'file',
             unValidReason: '',
           },
-        ]).map(({ unValidReason }) => unValidReason)
+        ])
+          .map(({ unValidReason }) => unValidReason)
+          .join('，');
 
-        isRoom = !!+isRoom /** "1" => true , "0" => false */
+        isRoom = !!+isRoom; /** "1" => true , "0" => false */
 
         // 支持jsonLike传递备注名 {alias: 123}
         if (/{\s*"?'?alias"?'?\s*:[^}]+}/.test(to)) {
           try {
-            to = Utils.parseJsonLikeStr(to)
+            to = Utils.parseJsonLikeStr(to);
           } catch (e) {
             unValidParamsStr = [
               `to 参数发消息给备注名， json string 格式不正确`,
-            ].concat(unValidParamsStr)
+            ].concat(unValidParamsStr);
           }
         }
-        unValidParamsStr = unValidParamsStr.join('，')
+        unValidParamsStr = unValidParamsStr.join('，');
 
         // json
       } else {
-        to = req.body.to
-        isRoom = req.body.isRoom || false
-        type = req.body.type
-        content = req.body.content
+        to = req.body.to;
+        isRoom = req.body.isRoom || false;
+        type = req.body.type;
+        content = req.body.content;
 
         // 校验必填参数
         unValidParamsStr = Utils.getUnValidParamsList([
@@ -118,49 +120,59 @@ module.exports = function registerPushHook({ app, bot }) {
           },
         ])
           .map(({ unValidReason }) => unValidReason)
-          .join('，')
+          .join('，');
       }
 
       if (unValidParamsStr) {
         return res.status(200).json({
           success: false,
           message: `[${unValidParamsStr}] params  is not valid, please checkout the api reference (https://github.com/danni-cool/docker-wechatbot-webhook#%EF%B8%8F-api)`,
-        })
+        });
       }
 
-      const senderName = getSenderName(to)
+      const senderName = getSenderName(to, isRoom);
 
-      const receiverNames = Array.isArray(to) ? to : [to]
+      const receiverNames = Array.isArray(to) ? to : [to];
       const msgReceivers = isRoom
-        ? await Promise.all(receiverNames.map((name) => bot.Room.find({ topic: name })))
-        : await Promise.all(receiverNames.map((name) => bot.Contact.find(Utils.equalTrueType(name, 'object') ? name : { name })))
+        ? await Promise.all(
+            receiverNames.map((name) => bot.Room.find({ topic: name }))
+          )
+        : await Promise.all(
+            receiverNames.map((name) =>
+              bot.Contact.find(
+                Utils.equalTrueType(name, 'object') ? name : { name }
+              )
+            )
+          );
 
-      const sendStatuses = await Promise.all(msgReceivers.map((msgReceiver) => {
-        if (msgReceiver) {
-          return Service.formatAndSendMsg({
-            bot,
-            type,
-            content,
-            msgInstance: msgReceiver,
-            res,
-          })
-        } else {
-          return false
-        }
-      }))
+      const sendStatuses = await Promise.all(
+        msgReceivers.map((msgReceiver) => {
+          if (msgReceiver) {
+            return Service.formatAndSendMsg({
+              bot,
+              type,
+              content,
+              msgInstance: msgReceiver,
+              res,
+            });
+          } else {
+            return false;
+          }
+        })
+      );
 
-      const success = sendStatuses.every((status) => status)
+      const success = sendStatuses.every((status) => status);
       if (success) {
         res.status(200).json({
           success: true,
           message: `Message sent successfully.`,
-        })
+        });
       } else {
         res.status(200).json({
           success: false,
           message: `${isRoom ? 'Room' : 'User'} is not found`,
-        })
+        });
       }
-    }),
-  )
-}
+    })
+  );
+};
